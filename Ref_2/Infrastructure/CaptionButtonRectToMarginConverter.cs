f5c5b876d785df2431b44b8187b85aebd783e621
Wiktor Wijata <wiktorwijata@gmail.com>
2018-07-23 12:25:54 +0200

    GUI gotowe oraz podzielone na MVVM

---
+++ Ref_2/Infrastructure/CaptionButtonRectToMarginConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Ref_2.Infrastructure
+{
+    public class CaptionButtonRectToMarginConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var captionLocation = (Rect)value;
+
+            return new Thickness(0, captionLocation.Top + 6, -captionLocation.Right, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
